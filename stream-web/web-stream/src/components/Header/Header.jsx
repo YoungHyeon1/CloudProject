8040d2b0fe5a1e6d49b760e978d68441c1e509cc
Your Name <you@example.com>
2023-12-09 17:58:03 +0900

    comment에 따른 코드 수정 & CSS 일부 수정

--- stream-web/web-stream/src/components/Header/Header.jsx
+++ stream-web/web-stream/src/components/Header/Header.jsx
@@ -1,43 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useAsyncError } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import { useAuth } from "../AppProvider";
 import "./Header.css";
 import axios from 'axios';
 
 const Header = () => {
-  /**
-   * 로그인이 되었는지는 AppProvider 의 Props입니다.
-   * isLogin은 boolean 입니다.
-   */
-  const { isLogin} = useAuth();
-  const [profileImg, setProfileImg] = useState();
-  
-  useEffect(()=> {
-    axios.get('https://xw6vimxva3.execute-api.ap-northeast-2.amazonaws.com/develop/public/users')
-    .then(response => {
-      const currentNickname = sessionStorage.getItem("nickname");
+  const { isLogin } = useAuth();
+  const currentNickname = sessionStorage.getItem("chanelName");
+  const [profileImg, setProfileImg] = useState("");
 
-      const userProfile = response.data.find(user => user.nickname === currentNickname);
-
-      if(userProfile){
-        setProfileImg(userProfile.profile);
-      }
-    }) .catch(error => {
-      console.error(error);
-    });
-  }, []);
+  useEffect(() => {
+    if (isLogin && currentNickname) {
+      axios.get('https://xw6vimxva3.execute-api.ap-northeast-2.amazonaws.com/develop/public/users', {
+        params: {
+          getProfile: currentNickname
+        }
+      })
+      .then(response => {
+        const userProfile = response.data;
+        if (userProfile) {
+          setProfileImg(userProfile.profile);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  }, [isLogin, currentNickname]);
 
   return (
     <header className="site-header">
       <Link to="/" className="logo">
-        <img src="/images/Mainlogo.png" className="mainlogo"></img>
+        <img src="/images/Mainlogo.png" alt="Main Logo" className="mainlogo" />
       </Link>
       <nav className="main-nav">
-        <Link to="https://github.com/YoungHyeon1/CloudProject" className="profile">Github</Link>
+        <a href="https://github.com/YoungHyeon1/CloudProject" className="profile">Github</a>
         {isLogin ? (
-        <Link to="/mypage">
-          <img src={profileImg} className='profileImg'></img>
-        </Link>
+          <Link to="/mypage">
+            <img src={profileImg} className='profileImg' />
+          </Link>
         ) : (
           <Link to="/login" className="loginbtn">로그인</Link>
         )}
