0326587a47f081f7bc7c22442bd32bdb17635ddf
Your Name <you@example.com>
2023-12-09 16:54:31 +0900

    Header 프로필 이미지 생성

--- stream-web/web-stream/src/components/Header/Header.jsx
+++ stream-web/web-stream/src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useAsyncError } from "react-router-dom";
 import { useAuth } from "../AppProvider";
 import "./Header.css";
+import axios from 'axios';
 
 const Header = () => {
   /**
@@ -9,8 +10,22 @@ const Header = () => {
    * isLogin은 boolean 입니다.
    */
   const { isLogin} = useAuth();
+  const [profileImg, setProfileImg] = useState();
   
-  console.log(sessionStorage.getItem);
+  useEffect(()=> {
+    axios.get('https://xw6vimxva3.execute-api.ap-northeast-2.amazonaws.com/develop/public/users')
+    .then(response => {
+      const currentNickname = sessionStorage.getItem("nickname");
+
+      const userProfile = response.data.find(user => user.nickname === currentNickname);
+
+      if(userProfile){
+        setProfileImg(userProfile.profile);
+      }
+    }) .catch(error => {
+      console.error(error);
+    });
+  }, []);
 
   return (
     <header className="site-header">
@@ -21,7 +36,8 @@ const Header = () => {
         <Link to="https://github.com/YoungHyeon1/CloudProject" className="profile">Github</Link>
         {isLogin ? (
         <Link to="/mypage">
-          {sessionStorage.getItem("nickname")} </Link>
+          <img src={profileImg} className='profileImg'></img>
+        </Link>
         ) : (
           <Link to="/login" className="loginbtn">로그인</Link>
         )}
